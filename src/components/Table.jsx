23188d2163aff03eddaaeb30730bd1df1c49a220
Luis Avecilla <avecillalu92@gmail.com>
2024-03-21 01:30:19 -0400

    done

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,15 +1,29 @@
+import React from "react";
+import './styling.css';
+
 const Table = ({ sat }) => {
+  if (!sat || !Array.isArray(sat)) {
+    return <div>No Satellite data available</div>
+  }
   return (
     <table>
     <thead>
      <tr>
-       <th>Header TBD</th>
+       <th>Name</th>
+       <th>Type of Satellite</th>
+       <th>Launch Date</th>
+       <th> Satus</th>
      </tr>
      </thead>
      <tbody>
-     <tr>
-       <td>Row Data TBD</td>
+      {sat.map((satellite) =>(
+     <tr key={satellite.id}>
+       <td>{satellite.name}</td>
+       <td>{satellite.type}</td>
+       <td>{satellite.launchDate}</td>
+       <td>{satellite.operational ? 'Active' : 'Inactive'}</td>
      </tr>
+      ))} 
      </tbody>
    </table>
   );
